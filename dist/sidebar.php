79aba7f68bbacb9385137958190225c3c77f4c70
ypwan30 <ath.ks.xss@gmail.com>
2025-07-23 19:43:25 +0700

    ha

--- dist/sidebar.php
+++ dist/sidebar.php
@@ -1,4 +1,4 @@
-      <aside class="app-sidebar bg-body-secondary shadow" data-bs-theme="dark">
+<aside class="app-sidebar bg-body-secondary shadow" data-bs-theme="dark">
         <!--begin::Sidebar Brand-->
         <div class="sidebar-brand">
           <!--begin::Brand Link-->
